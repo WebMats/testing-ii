f00f9f79cfac309b5fe441686e9a8fa1b066a3b5
WebMats <lorlando127@gmail.com>
2019-03-05 17:18:40 -0500

    built Display component

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const display = () => {
+const display = ({player}) => {
   return (
-    <div>
-      
-    </div>
+    <section>
+      <h3>Status of Player</h3>
+      <p className="balls">Balls: <span data-testid="ball-count">{player.balls}</span></p>
+      <p className="strikes">Stikes: <span data-testid="strike-count">{player.strikes}</span></p>
+    </section>
   )
 }
 
--- src/components/Display/Display.test.js
+++ src/components/Display/Display.test.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import { render, cleanup } from 'react-testing-library';
 import Display from './Display';
+import 'jest-dom/extend-expect'
 
+
+afterEach(cleanup) 
 
 test('should display the count of balls and strikes for the at-bat', () => {
   const playerDisplayed = { name: 'player2', balls:0, strikes:0 , atBat: true };
-  const { getByText } = render(<Display player={playerDisplayed} />);
-  const ballCount = getByText(/ball-count/i);
-  const strikeCount = getByText(/strike-count/i);
+  const { getByTestId } = render(<Display player={playerDisplayed} />);
+  const ballCount = getByTestId(/ball-count/i);
+  const strikeCount = getByTestId(/strike-count/i);
   expect(+strikeCount.textContent).toEqual(playerDisplayed.strikes);
   expect(+ballCount.textContent).toEqual(playerDisplayed.balls);
 })
 
 test('should be updated when user records activity on Dashboard', () => {
     const playerDisplayed = { name: 'player2', balls:0, strikes:0 , atBat: true };
-    const { getByText, rerender } = render(<Display player={playerDisplayed} />);
-    const ballCount = +getByText(/ball-count/i).textContent;
-    const strikeCount = +getByText(/strike-count/i).textContent;
+    const { getByTestId, rerender } = render(<Display player={playerDisplayed} />);
+    const ballCount = +getByTestId(/ball-count/i).textContent;
+    const strikeCount = +getByTestId(/strike-count/i).textContent;
     const updatedPlayerDisplayed = { name: 'player2', balls:2, strikes:1 , atBat: true };
     rerender(<Display player={updatedPlayerDisplayed} />)
-    const updatedBallCount = +getByText(/ball-count/i).textContent;
-    const updatedStrikeCount = +getByText(/strike-count/i).textContent;
+    const updatedBallCount = +getByTestId(/ball-count/i).textContent;
+    const updatedStrikeCount = +getByTestId(/strike-count/i).textContent;
     expect(updatedBallCount).toEqual(ballCount + 2);
     expect(updatedStrikeCount).toEqual(strikeCount + 1);
 })
