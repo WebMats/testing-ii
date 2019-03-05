3020ef2bbfe7cad3569ab743ec35155c5daa3106
WebMats <lorlando127@gmail.com>
2019-03-05 17:55:04 -0500

    setup Dashboard component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import React from 'react'
 const dashboard = (props) => {
   return (
     <div>
-    
+      <button onClick={props.clicked} data-testid="submit-btn">Click To Update</button>
     </div>
   )
 }
