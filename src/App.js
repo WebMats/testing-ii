a7ff3030b557b25398c44a09c6189d3a85b51575
WebMats <lorlando127@gmail.com>
2019-03-05 16:16:16 -0500

    added basic react setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const players = [
+  { name: 'player1', balls:0, strikes:0 },
+  { name: 'player2', balls:0, strikes:0 },
+  { name: 'player3', balls:0, strikes:0 },
+  { name: 'player4', balls:0, strikes:0 },
+]
+
+const app = () => {
+    return (
+      <div className="App">
+      </div>
+    );
+}
+
+export default app;
