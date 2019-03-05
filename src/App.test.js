09227a7652a1b6ca98df9bc36590b67e1a51c5f0
WebMats <lorlando127@gmail.com>
2019-03-05 17:51:19 -0500

    added test for Dashboard component

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,4 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  const { getByText } = render(<App />)
 });
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const dashboard = () => {
+const dashboard = (props) => {
   return (
     <div>
-      
+    
     </div>
   )
 }
